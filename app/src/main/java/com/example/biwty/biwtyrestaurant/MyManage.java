a111f417c1a250344e931f9bbafd6e21b42da83c
biw <sunantha147@gmail.com>
2016-03-19 11:55:37 +0700

    Delete value SQLite finish

--- app/src/main/java/com/example/biwty/biwtyrestaurant/MyManage.java
+++ app/src/main/java/com/example/biwty/biwtyrestaurant/MyManage.java
@@ -1,5 +1,6 @@
 package com.example.biwty.biwtyrestaurant;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -12,6 +13,19 @@ public class MyManage {
     private MyOpenHelper myOpenHelper;
     private SQLiteDatabase writeSqLiteDatabase, readSqLiteDatabase;
 
+    public static final String user_table = "userTABLE";
+    public static final String column_id = "_id";
+    public static final String column_User = "User";
+    public static final String column_Password = "Password";
+    public static final String column_Name = "Name";
+
+
+    public static final String food_table = "foodTABEL";
+    public static final String column_Food = "Food";
+    public static final String column_Price = "Price";
+    public static final String column_Source = "Source";
+
+
     public MyManage(Context context) {
         //Create &Connected SQLite
         myOpenHelper = new MyOpenHelper(context);
@@ -20,6 +34,42 @@ public class MyManage {
 
 
     }//Constructor
+
+
+    public long addValue(int intTABLE,
+                         String strColumn2,
+                         String strColumn3,
+                         String strColumn4){
+
+        ContentValues contentValues = new ContentValues();
+
+        long myLong = 0;
+
+        switch (intTABLE) {
+            case 1:
+                //For userTABLE
+                contentValues.put(column_User, strColumn2);
+                contentValues.put(column_Password, strColumn3);
+                contentValues.put(column_Name, strColumn4);
+
+                myLong = writeSqLiteDatabase.insert(user_table, null, contentValues);
+
+                break;
+            case 2:
+                //For foodTABLE
+                contentValues.put(column_Food ,strColumn2);
+                contentValues.put(column_Price,strColumn3);
+                contentValues.put(column_Source,strColumn4);
+
+                myLong = writeSqLiteDatabase.insert(food_table, null, contentValues);
+
+
+                break;
+        }
+
+
+        return 0;
+    }
 
 
 }//Main Class
